cca7106aa93ddda281a14b61da6a7d4a1ae44870
Rocio Sanchez <sanchezmaribel.ro@gmail.com>
2024-10-12 19:37:40 +0000

    ADD: Agrego el Punto A y B.

---
+++ RTA_SCRIPT_Examen_20241012/Punto_A.sh
@@ -0,0 +1,4 @@
+#Agrego en el directorio raíz del sistema operativo lo designado en el punto A.
+
+sudo mkdir -p $HOME/Examenes-UTN/{alumno_{1..3}/parcial_{1..3},profesores}
+
---
+++ RTA_SCRIPT_Examen_20241012/Punto_B.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+#creamos un txt para que se pase por parametros al fdisk
+cat > comandos_fdisk.txt <<EOF
+
+n
+p
+
+
++1GB
+n
+e
+
+
+
+n
+
++1GB
+n
+
++1GB
+n
+
++1GB
+n
+
++1GB
+n
+
++1GB
+n
+
++1GB
+n
+
++1GB
+n
+
++1GB
+n
+
++1GB
+w
+EOF
+#se crean las particiones
+sudo fdisk /dev/sdc < comandos_fdisk.txt
+#formateamos
+sudo partprobe /dev/sdc
+sudo mkfs.ext4 /dev/sdc1
+sudo mkfs.ext4 /dev/sdc5
+sudo mkfs.ext4 /dev/sdc6
+sudo mkfs.ext4 /dev/sdc7
+sudo mkfs.ext4 /dev/sdc8
+sudo mkfs.ext4 /dev/sdc9
+sudo mkfs.ext4 /dev/sdc10
+sudo mkfs.ext4 /dev/sdc11
+sudo mkfs.ext4 /dev/sdc12
+sudo mkfs.ext4 /dev/sdc13
+#modificamos el fstab) para que quede de manera persistente.
+echo "/dev/sdc1 /Examenes-UTN/alumno_1/parcial_1 ext4 defaults 0 0" | sudo tee -a /etc/fstab
+echo "/dev/sdc5 /Examenes-UTN/alumno_1/parcial_2 ext4 defaults 0 0" | sudo tee -a /etc/fstab
+echo "/dev/sdc6 /Examenes-UTN/alumno_1/parcial_3 ext4 defaults 0 0" | sudo tee -a /etc/fstab
+echo "/dev/sdc7 /Examenes-UTN/alumno_2/parcial_1 ext4 defaults 0 0" | sudo tee -a /etc/fstab
+echo "/dev/sdc8 /Examenes-UTN/alumno_2/parcial_2 ext4 defaults 0 0" | sudo tee -a /etc/fstab
+echo "/dev/sdc9 /Examenes-UTN/alumno_2/parcial_3 ext4 defaults 0 0" | sudo tee -a /etc/fstab
+echo "/dev/sdc10 /Examenes-UTN/alumno_3/parcial_1 ext4 defaults 0 0" | sudo tee -a /etc/fstab
+echo "/dev/sdc11 /Examenes-UTN/alumno_3/parcial_2 ext4 defaults 0 0" | sudo tee -a /etc/fstab
+echo "/dev/sdc12 /Examenes-UTN/alumno_3/parcial_3 ext4 defaults 0 0" | sudo tee -a /etc/fstab
+echo "/dev/sdc13 /Examenes-UTN/profesores ext4 defaults 0 0" | sudo tee -a /etc/fstab
+sudo monut -a
