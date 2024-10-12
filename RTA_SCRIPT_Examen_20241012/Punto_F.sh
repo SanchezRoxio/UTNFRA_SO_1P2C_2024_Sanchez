6ce4e70dff37d8642fe706f3b42e376648ee3835
Rocio Sanchez <sanchezmaribel.ro@gmail.com>
2024-10-12 21:58:17 +0000

    ADD: Agrego correcciones al Punto F.

--- RTA_SCRIPT_Examen_20241012/Punto_F.sh
+++ RTA_SCRIPT_Examen_20241012/Punto_F.sh
@@ -1,9 +1,12 @@
-
+#Creo una variable donde guardar.
 ARCHIVO="../RTA_ARCHIVOS_Examen_20241012/Filtro_Avanzado.txt"
 
+#con touch se crea:
 touch $ARCHIVO
+
 echo "Mi IP Publica es: $(curl -s ifconfig.me)" > $ARCHIVO
 
+#Luego se crean las demas variables y las asigno a el archivo creado anteriormente
 USUARIO=$(whoami) 
 echo "Mi usuario es: $USUARIO" >> $ARCHIVO
 
@@ -13,4 +16,5 @@ echo "El Hash de mi usuario es: $HASH" >> $ARCHIVO
 REPOS=$(git remote get-url origin)
 echo "La URL de mi repositorio es: $REPOS" >> $ARCHIVO
 
+#muestro con un cat:
 cat $ARCHIVO
